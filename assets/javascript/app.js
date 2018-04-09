5eb977eff53f680e1e59b0c6ebbddcf31accb6ff
janell <janell.williams@ticketmaster.com>
2018-04-08 19:12:17 -0700

    finished

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -16,11 +16,15 @@ $(document).ready(function () {});
   var database = firebase.database();
 
   // Initial Values
-  var trainName = "";
-  var destination = "";
-  var firstTrainTime = "";
-  var frequency = 0;
-  var nextArrival = "";
+  var trainName;
+  var destination;
+  var firstTrainTime;
+  var frequency;
+  var nextArrival;
+  var minutes;
+  var maxMoment;
+
+ 
 
    // Capture Button Click
    $("#add-train").on("click", function(event) {
@@ -31,6 +35,37 @@ $(document).ready(function () {});
     destination = $("#destination").val().trim();
     firstTrainTime = $("#trainTime").val().trim();
     frequency = $("#frequency").val().trim();
+
+    //Calculations for arrival time
+    //var maxMoment = moment.max(moment(), firstTrainTime);
+    //console.log(maxMoment);
+    var now = moment().unix();
+    console.log("this is now" + now);
+    var timeArr = firstTrainTime.split(":");
+    var TrainTime = moment().hours(timeArr[0]).minutes(timeArr[1]);
+    console.log("Traintime" + TrainTime);
+    var timedifference = moment().diff(TrainTime, "minutes");
+    console.log("timed difference"  + timedifference);
+    var remainder = timedifference % frequency; 
+    console.log("remainder" + remainder);
+    minutes = frequency - remainder;
+    console.log("minutes" + minutes);
+    nextArrival = moment().add(minutes, "m").format("hh:mm A");
+    console.log("arrival" + nextArrival);
+
+   // convert time remaining before next train 
+   var newArrival = moment(nextArrival).format("hh:mm");
+   var anotherformatarrival = newArrival.split(":");
+   var arrivaltotalminutes =  moment().hours(newArrival[0]).minutes(newArrival[1]);
+   console.log("Arrival in minutes" + arrivaltotalminutes);
+   var nowMin = moment().format("mm");
+   console.log("Now" + now);
+   var RemainingMinutes = arrivaltotalminutes - moment;
+   console.log("Reamining minutes" + RemainingMinutes);
+   console.log(nextArrival);
+   var a = moment(nextArrival);
+   console.log("A" +  a);
+
 
      // Code for handling the push
      database.ref().push({
@@ -53,7 +88,7 @@ $(document).ready(function () {});
       var tf = $("<td>").text(frequency);
       var na = $("<td>").text(nextArrival);
    
-      $(".table").append(trainRow, tn, td, tf, na, EndRow );
+      $("tbody").append(trainRow, tn, td, tf, na, EndRow );
 
 
     });
@@ -77,13 +112,3 @@ $(document).ready(function () {});
       console.log("Errors handled: " + errorObject.code);
     });
 
-
-    
-
-
-//     <tr>
-//     <th scope="row">3</th>
-//     <td>Larry</td>
-//     <td>the Bird</td>
-//     <td>@twitter</td>
-//   </tr>
